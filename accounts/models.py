d021de60e0ec0aa6829b69932b6edd18be5f1e1f
raj vamsy <rajvamsypro@gmail.com>
2024-08-28 09:52:03 +0530

    models updated

--- accounts/models.py
+++ accounts/models.py
@@ -98,26 +98,3 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.email
-    
-  
-
-    
-@receiver(post_save, sender=User)
-def post_save_create_profile_reciever(sender,instance,created,**kwargs):
-    #post_save.connect(post_save_create_profile_reciever,sender=User)
-    print(created)
-    if created:
-        UserProfile.objects.create(user=instance)
-        print('create the user profile')
-    
-    else:
-        try:
-            profile = UserProfile.objects.get(user=instance)
-            profile.save()
-           
-        except Exception:
-            # create the user profile if not exist
-            UserProfile.objects.create(user=instance)
-            print('Profile was not existed, but we created one')
-                
-        print('user is updated')
